c19c36d350d6364f97d65f93148b4ccf8a3392fa
valencra <valencra@mcmaster.ca>
2018-02-25 20:15:53 -0500

    Add abstract base entity

---
+++ src/main/java/com/valencra/recipe/core/BaseEntity.java
@@ -0,0 +1,38 @@
+package com.valencra.recipe.core;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Version
+  private Long version;
+
+  protected BaseEntity() {
+    id = null;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getVersion() {
+    return version;
+  }
+
+  public void setVersion(Long version) {
+    this.version = version;
+  }
+}

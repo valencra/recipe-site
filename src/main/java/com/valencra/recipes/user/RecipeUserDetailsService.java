23d75e4283dfcfdbec57db88a49f1f43784aa80c
valencra <valencra@mcmaster.ca>
2018-02-28 08:27:53 -0500

    Add recipe user details service

--- src/main/java/com/valencra/recipes/user/RecipeUserDetailsService.java
+++ src/main/java/com/valencra/recipes/user/RecipeUserDetailsService.java
@@ -1,10 +1,13 @@
 package com.valencra.recipes.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class RecipeUserDetailsService implements UserDetailsService{
@@ -13,7 +16,19 @@ public class RecipeUserDetailsService implements UserDetailsService{
   RecipeUserService recipeUserService;
 
   @Override
+  @Transactional(readOnly = true)
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return null;
+    RecipeUser recipeUser = recipeUserService.findByUsername(username);
+
+    if (recipeUser == null) {
+      throw new UsernameNotFoundException("Unable to find user with username " + username);
+    }
+
+    User user = new User(
+        recipeUser.getUsername(),
+        recipeUser.getPassword(),
+        AuthorityUtils.createAuthorityList(recipeUser.getRoles()));
+
+    return user;
   }
 }

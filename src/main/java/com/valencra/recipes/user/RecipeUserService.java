f2aa7e25b73439692b1e9da544ce170ea25e47c6
valencra <valencra@mcmaster.ca>
2018-02-28 08:14:37 -0500

    Add missing service annotation

--- src/main/java/com/valencra/recipes/user/RecipeUserService.java
+++ src/main/java/com/valencra/recipes/user/RecipeUserService.java
@@ -1,9 +1,11 @@
 package com.valencra.recipes.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class RecipeUserService implements UserService {
 
   @Autowired

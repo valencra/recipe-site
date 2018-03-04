e016ea08615dff8fb0cb887caff8f2e117f7353a
valencra <valencra@mcmaster.ca>
2018-03-04 09:17:45 -0500

    Add security configuration class

---
+++ src/main/java/com/valencra/recipes/config/SecurityConfig.java
@@ -0,0 +1,85 @@
+package com.valencra.recipes.config;
+
+import com.valencra.recipes.model.User;
+import com.valencra.recipes.service.RecipesAppUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.query.spi.EvaluationContextExtension;
+import org.springframework.data.repository.query.spi.EvaluationContextExtensionSupport;
+import org.springframework.security.access.expression.SecurityExpressionRoot;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Autowired
+  private RecipesAppUserDetailsService recipesAppUserDetailsService;
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(recipesAppUserDetailsService)
+        .passwordEncoder(User.PASSWORD_ENCODER);
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/assets/**");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .antMatchers("/signup").permitAll()
+        .anyRequest().permitAll()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .permitAll()
+        .successHandler(loginSuccessHandler())
+        .failureHandler(loginFailureHandler())
+        .and()
+        .logout()
+        .permitAll()
+        .logoutSuccessUrl("/login")
+        .and()
+        .csrf().disable();
+
+    http.headers().frameOptions().disable();
+  }
+
+  public AuthenticationSuccessHandler loginSuccessHandler() {
+    return (request, response, authentication) -> response.sendRedirect("/");
+  }
+
+  public AuthenticationFailureHandler loginFailureHandler() {
+    return (request, response, exception) -> response.sendRedirect("/login");
+  }
+
+  @Bean
+  public EvaluationContextExtension securityExtension() {
+    return new EvaluationContextExtensionSupport() {
+      @Override
+      public String getExtensionId() {
+        return "security";
+      }
+
+      @Override
+      public Object getRootObject() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return new SecurityExpressionRoot(authentication) {};
+      }
+    };
+  }
+}

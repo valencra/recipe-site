d64c4fc7f6cb1bc968eee0b3e4e437064e754199
valencra <valencra@mcmaster.ca>
2018-03-25 19:00:19 -0400

    Add user profile routes

--- src/main/java/com/valencra/recipes/web/controller/UserController.java
+++ src/main/java/com/valencra/recipes/web/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -36,5 +37,26 @@ public class UserController {
   public String login(Model model) {
     model.addAttribute("user", new User());
     return "login";
+  }
+
+  @GetMapping("/profile")
+  public String currentUserProfile(Model model) {
+    User currentUser = (User) model.asMap().get("currentUser");
+    model.addAttribute("user", currentUser);
+    model.addAttribute("authorized", true);
+    return "profile";
+  }
+
+  @GetMapping("/users/{id}")
+  public String userProfile(@PathVariable Long id, Model model) {
+    User queriedUser = userService.findOne(id);
+    model.addAttribute("user", queriedUser);
+
+    User currentUser = (User) model.asMap().get("currentUser");
+    if (currentUser != null && currentUser.isAdmin()) {
+      model.addAttribute("authorized", true);
+    }
+
+    return "profile";
   }
 }

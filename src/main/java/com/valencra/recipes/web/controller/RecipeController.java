f17fde3a143714cc6fa5c9c3c3f54688f0dee732
valencra <valencra@mcmaster.ca>
2018-03-25 22:12:19 -0400

    Add recipe index route

---
+++ src/main/java/com/valencra/recipes/web/controller/RecipeController.java
@@ -0,0 +1,40 @@
+package com.valencra.recipes.web.controller;
+
+import com.valencra.recipes.enums.Category;
+import com.valencra.recipes.model.Recipe;
+import com.valencra.recipes.model.User;
+import com.valencra.recipes.service.IngredientService;
+import com.valencra.recipes.service.RecipeService;
+import com.valencra.recipes.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class RecipeController {
+  @Autowired
+  private UserService userService;
+
+  @Autowired
+  private IngredientService ingredientService;
+
+  @Autowired
+  private RecipeService recipeService;
+
+  @GetMapping("/")
+  public String recipeIndex(Model model) {
+    User currentUser = (User) model.asMap().get("currentUser");
+    if (currentUser != null) {
+      model.addAttribute("authorized", currentUser.isAdmin());
+    }
+
+    List<Recipe> recipes = recipeService.findAll();
+    model.addAttribute("recipes", recipes);
+    model.addAttribute("categories", Category.values());
+
+    return "index";
+  }
+}

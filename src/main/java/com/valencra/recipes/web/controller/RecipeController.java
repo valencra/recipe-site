0c61f8efe69daaaf7dc7cebe3f18c81393efce0b
valencra <valencra@mcmaster.ca>
2018-03-28 06:42:21 -0400

    Add recipe search route

--- src/main/java/com/valencra/recipes/web/controller/RecipeController.java
+++ src/main/java/com/valencra/recipes/web/controller/RecipeController.java
@@ -13,8 +13,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class RecipeController {
@@ -28,7 +31,7 @@ public class RecipeController {
   private RecipeService recipeService;
 
   @GetMapping("/")
-  public String recipeIndex(Model model) {
+  public String recipesIndex(Model model) {
     User currentUser = (User) model.asMap().get("currentUser");
     if (currentUser != null) {
       model.addAttribute("authorized", currentUser.isAdmin());
@@ -46,6 +49,41 @@ public class RecipeController {
     Recipe recipe = recipeService.findOne(id);
     model.addAttribute("recipe", recipe);
     return "detail";
+  }
+
+  @GetMapping("/recipes/search")
+  public String searchRecipes(
+      @RequestParam(required = false) String query,
+      @RequestParam(required = false) String field,
+      @RequestParam(required = false) String category,
+      Model model) {
+    List<Recipe> recipes = recipeService.findAll();
+
+    if (query != null && query.trim() != "") {
+      switch (field) {
+        case "name":
+          recipes = recipeService.findByNameContaining(query);
+          break;
+        case "description":
+          recipes = recipeService.findByDescriptionContaining(query);
+          break;
+        case "ingredient":
+          recipes = recipeService.findByIngredient(query);
+          break;
+      }
+
+      if (category != null && category.trim() != "") {
+        recipes = recipes.stream()
+            .filter(recipe -> recipe.getCategory().equals(category))
+            .collect(Collectors.toList());
+      }
+    }
+
+    model.addAttribute("recipes", recipes);
+    model.addAttribute("categories", Category.values());
+
+    return "index";
+
   }
 
   @GetMapping("/recipes/create-form")

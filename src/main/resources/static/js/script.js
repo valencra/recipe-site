86d313ba02829956fdcd86906bfb12c0e5f18283
valencra <valencra@mcmaster.ca>
2018-04-08 15:03:01 -0400

    Update index reference

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,13 +2,13 @@ $('#add-ingredient').click(function() {
     var ingredientCount = $('.ingredient-row').length;
     var ingredientRow =  '<div class="ingredient-row">' +
                          '<div class="prefix-20 grid-30">' +
-                         '<p> <input type="text" id="ingredients' + index + '.item" name="ingredients[' + index + '].name" />' +
+                         '<p> <input type="text" id="ingredients' + ingredientCount + '.item" name="ingredients[' + ingredientCount + '].name" />' +
                          '</p> </div>' +
                          '<div class="grid-30">' +
-                         '<p> <input type="text" id="ingredients' + index + '.condition" name="ingredients[' + index + '].condition" />' +
+                         '<p> <input type="text" id="ingredients' + ingredientCount + '.condition" name="ingredients[' + ingredientCount + '].condition" />' +
                          '</p> </div>' +
                          '<div class="grid-10 suffix-10">' +
-                         '<p> <input type="text" id="ingredients' + index + '.quantity" name="ingredients[' + index + '].quantity" />' +
+                         '<p> <input type="text" id="ingredients' + ingredientCount + '.quantity" name="ingredients[' + ingredientCount + '].quantity" />' +
                          '</p> </div>' +
                          '</div>'
     $("#add-ingredient-row" ).before(newRow);
@@ -19,7 +19,7 @@ $('#add-step').click(function() {
     var stepRow =  '<div class="step-row">' +
                    '<div class="prefix-20 grid-80">' +
                    '<p>' +
-                   '<input  type="text" id="steps' + index + '.stepName" name="steps[' + index + '].stepName" />' +
+                   '<input  type="text" id="steps' + stepCount + '.stepName" name="steps[' + stepCount + '].stepName" />' +
                    '</p>' +
                    '</div>' +
                    '</div>'

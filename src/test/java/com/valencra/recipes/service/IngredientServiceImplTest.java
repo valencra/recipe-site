95aab080a7d04a517715f10a7a13960443864266
valencra <valencra@mcmaster.ca>
2018-04-12 06:35:12 -0400

    Add ingredient service test

---
+++ src/test/java/com/valencra/recipes/service/IngredientServiceImplTest.java
@@ -0,0 +1,60 @@
+package com.valencra.recipes.service;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import com.valencra.recipes.model.Ingredient;
+import com.valencra.recipes.repository.IngredientRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IngredientServiceImplTest {
+  @Mock
+  private IngredientRepository ingredientRepository;
+
+  @InjectMocks
+  private IngredientService ingredientService = new IngredientServiceImpl();
+
+  @Test
+  public void findAll() {
+    List<Ingredient> expectedIngredients = Arrays.asList(new Ingredient("Ingredient", "Condition", 1.0));
+    when(ingredientRepository.findAll())
+        .thenReturn(expectedIngredients);
+
+    List<Ingredient> actualIngredients = ingredientService.findAll();
+
+    assertEquals(expectedIngredients, actualIngredients);
+  }
+
+  @Test
+  public void findOne() {
+    Ingredient expectedIngredient = new Ingredient("Ingredient", "Condition", 1.0);
+    long testId = 1L;
+    when(ingredientRepository.findOne(testId))
+        .thenReturn(expectedIngredient);
+
+    Ingredient actualIngredient = ingredientService.findOne(testId);
+
+    assertEquals(expectedIngredient, actualIngredient);
+  }
+
+  @Test
+  public void findByName() {
+    Ingredient expectedIngredient = new Ingredient("Ingredient", "Condition", 1.0);
+    String testName = "Ingredient";
+    when(ingredientRepository.findByName(testName))
+        .thenReturn(expectedIngredient);
+
+    Ingredient actualIngredient = ingredientService.findByName(testName);
+
+    assertEquals(expectedIngredient, actualIngredient);
+  }
+}

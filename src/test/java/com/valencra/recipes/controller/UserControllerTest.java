ae9e27a62b9d1be2942530737439694be1ca8155
valencra <valencra@mcmaster.ca>
2018-04-18 00:51:04 -0400

    Add sign up post method test

--- src/test/java/com/valencra/recipes/controller/UserControllerTest.java
+++ src/test/java/com/valencra/recipes/controller/UserControllerTest.java
@@ -5,7 +5,9 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -21,6 +23,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -72,5 +75,18 @@ public class UserControllerTest {
         .andExpect(status().isOk())
         .andExpect(view().name("signup"))
         .andExpect(model().attribute("user", new User()));
+  }
+
+  @Test
+  public void postSignupAddsUser() throws Exception  {
+    when(userService.findByUsername(TEST_USERNAME)).thenReturn(null);
+
+    mockMvc.perform(post("/signup")
+        .param("name", TEST_USER.getName())
+        .param("username", TEST_USER.getUsername())
+        .param("password", TEST_USER.getPassword())
+        .param("roles", TEST_USER.getRoles()))
+        .andExpect(redirectedUrl("/login"))
+        .andExpect(status().is3xxRedirection());
   }
 }
